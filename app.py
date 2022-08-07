7b60150b95e37ecdaacf6107b60eea4a431da0b7
Nomis89 <95224816+Nomis89@users.noreply.github.com>
2022-08-07 12:02:41 +0200

    tweede commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World! Wh00pwh00p</p>"
 
 @app.route("/tweede")
 def hello_world2():
